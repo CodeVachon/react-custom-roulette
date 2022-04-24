05fbf977ad1c4be4659975c7810190c38cc7b87b
Christopher Vachon <cvachon@fortyfournorth.ca>
2022-04-23 21:09:49 -0400

    Resolve explicit-module-boundary-types Error

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export const getRotationDegrees = (
   prizeNumber: number,
   numberOfPrizes: number
-) => {
+): number => {
   const degreesPerPrize = 360 / numberOfPrizes;
 
   const initialRotation = 43 + degreesPerPrize / 2;
@@ -18,5 +18,5 @@ export const getRotationDegrees = (
     : prizeRotation;
 };
 
-export const clamp = (min: number, max: number, val: number) =>
+export const clamp = (min: number, max: number, val: number): number =>
   Math.min(Math.max(min, +val), max);
